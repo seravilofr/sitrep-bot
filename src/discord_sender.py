528dd6eb66fd50f9f3315fb2059599d92e23a6ce
François Olivares <156725976+seravilofr@users.noreply.github.com>
2026-03-17 10:03:03 +0100

    Add function to send messages to Discord

--- src/discord_sender.py
+++ src/discord_sender.py
@@ -1,1 +1,23 @@
+import os
+import requests
 
+
+def send_to_discord(message: str):
+
+    webhook_url = os.getenv("DISCORD_WEBHOOK_URL")
+
+    if not webhook_url:
+        print("Missing DISCORD_WEBHOOK_URL")
+        return
+
+    data = {
+        "content": message
+    }
+
+    response = requests.post(webhook_url, json=data)
+
+    if response.status_code == 204:
+        print("Message sent successfully")
+    else:
+        print(f"Failed to send message: {response.status_code}")
+        print(response.text)
