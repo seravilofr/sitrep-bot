bedb7b599392200dcd9aecf891dbf1876aa624ab
François Olivares <156725976+seravilofr@users.noreply.github.com>
2026-03-17 10:04:06 +0100

    Integrate Discord notification in main.py

--- src/main.py
+++ src/main.py
@@ -1,1 +1,12 @@
+from src.discord_sender import send_to_discord
 
+
+def main():
+
+    message = "🚀 SITREP Bot is now operational."
+
+    send_to_discord(message)
+
+
+if __name__ == "__main__":
+    main()
