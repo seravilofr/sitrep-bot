724258775bcd111b9c71d712e09ae53c6eb2cc2d
François Olivares <156725976+seravilofr@users.noreply.github.com>
2026-03-17 11:16:00 +0100

    Fix main function call and clean up code

--- src/main.py
+++ src/main.py
@@ -16,15 +16,4 @@ def main():
 
 
 if __name__ == "__main__":
-    main()from discord_sender import send_to_discord
-
-
-def main():
-
-    message = "🚀 SITREP Bot is now operational."
-
-    send_to_discord(message)
-
-
-if __name__ == "__main__":
     main()
